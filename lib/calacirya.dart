948be96bf69c6f71b588e3938726463f7fee4493
fatiharge <fatih@fatiharge.com>
2023-08-20 02:54:28 +0300

    secure örneği girildi main'e - secure için tekrar düşünelecek
    - async yapıda olduğu için bu fonksiyonlar için mutlaka loading sayfası olmalı ...
    
    - bu yüzden init işlemi loading de yapılmalı ve bu işlemlerden sonra start diyerek başlamalıdır.
    
    project init ->>>
    
    project start-> open ui ???

--- lib/calacirya.dart
+++ lib/calacirya.dart
@@ -1,8 +1,54 @@
 library calacirya;
 
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 
 class Calacirya {
-  Calacirya._();
+  static final Calacirya _calacirya = Calacirya._internal();
 
+  factory Calacirya() => _calacirya;
 
+  Calacirya._internal();
+
+  void start() {}
+
+  final _storage = const FlutterSecureStorage();
+
+  Future<void> _readAll() async {
+    final all = await _storage.readAll(
+      iOptions: _getIOSOptions(),
+      aOptions: _getAndroidOptions(),
+    );
+  }
+
+  Future<void> _deleteAll() async {
+    await _storage.deleteAll(
+      iOptions: _getIOSOptions(),
+      aOptions: _getAndroidOptions(),
+    );
+  }
+
+  Future<void> _addNewItem({required String key, required String value}) async {
+    await _storage.write(
+      key: key,
+      value: value,
+      iOptions: _getIOSOptions(),
+      aOptions: _getAndroidOptions(),
+    );
+  }
+
+  IOSOptions _getIOSOptions() => const IOSOptions(accountName: "calacirya");
+
+  AndroidOptions _getAndroidOptions() => const AndroidOptions(
+        encryptedSharedPreferences: true,
+        // sharedPreferencesName: 'Test2',
+        // preferencesKeyPrefix: 'Test'
+      );
+
+  Future<String?> _getItem({required String key}) async {
+    return await _storage.read(key: key);
+  }
 }
+
+enum _Actions { deleteAll }
+
+enum _ItemActions { delete, edit, containsKey, read }
