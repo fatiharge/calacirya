b997a2297b2c7de71cc2ce4a7c8b635d12967a1e
fatiharge <fatih@fatiharge.com>
2023-09-03 05:17:38 +0300

    Project update : 2023-09-03
    Signed-off-by: fatiharge <fatih@fatiharge.com>

--- lib/src/base_models/base_welcome_page_model.dart
+++ lib/src/base_models/base_welcome_page_model.dart
@@ -1,3 +1,13 @@
+/*
+ * Copyright (c) 2023 fatiharge
+ *
+ * You are free to copy, modify, and redistribute this software, but you must abide by these terms when distributing any derivative works under the project's license.
+ *
+ *  Author : Fatih Çetin
+ *  Mail   : fatih@fatiharge.com
+ *
+ */
+
 import 'package:calacirya/calacirya.dart';
 import 'package:flutter/material.dart';
 
