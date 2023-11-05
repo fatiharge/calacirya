099f5f1a1118d825db98da17ae8a1d8193e981af
fatiharge <fatih@fatiharge.com>
2023-11-05 15:27:38 +0300

    feat : update shared_keys_enum.dart
    Signed-off-by: fatiharge <fatih@fatiharge.com>

--- lib/enum/shared_keys_enum.dart
+++ lib/enum/shared_keys_enum.dart
@@ -10,15 +10,9 @@
 
 /// Enum representing different keys used for shared preferences.
 enum SharedKeys {
-  /// Represents the status of the welcome process.
-  welcomeStatus,
-
   /// Represents whether the welcome screen has been opened.
-  welcomeIsOpened,
-
-  /// Represents the auto-login setting.
-  autoLogin,
+  calaciryaWelcomeIsOpened,
 
   /// Represents the first login status.
-  firstLogin,
+  calacairyaFirstLogin,
 }
